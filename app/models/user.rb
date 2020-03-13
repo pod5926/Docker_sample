e27c22136b8fe8fcd7e2255258565c251be64b63
pod5926 <ks.apple.5926@gmail.com>
2020-03-13 22:03:45 +0900

    finish

---
+++ app/models/user.rb
@@ -0,0 +1,11 @@
+class User < ApplicationRecord
+  #:trackable,
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+  has_many :posts, dependent: :destroy
+  has_many :likes, dependent: :destroy
+  has_many :liked_posts, through: :likes, source: :post
+  def already_liked?(post)
+    self.likes.exists?(post_id: post.id)
+  end
+end
