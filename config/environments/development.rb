bd9786ed4b7dc408a9c8866083263aeeee05b946
pod5926 <ks.apple.5926@gmail.com>
2020-03-17 13:02:36 +0900

    config fix

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -59,4 +59,7 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+
+  #3/17
+  config.hosts << "peaceful-garden-74662.herokuapp.com"
 end
